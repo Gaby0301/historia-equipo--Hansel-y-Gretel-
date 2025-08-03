fe014994391c778ed7ab477a03e3abeb8257056e
Gabriela Ortiz <20243102532@uteq.edu.mx>
2025-08-02 23:24:41 -0600

    Agrega parte 1 de la historia

---
+++ Historia.py
@@ -0,0 +1,8 @@
+#Parte 1
+print("Hace mucho tiempo, vivían en un gran bosque un leñador pobre con su esposa y sus dos hijos.")
+print("El niño se llamaba Hansel y la niña Gretel. No tenían mucho que comer, y el hambre acechaba cada día más.")
+print("Una noche, cuando los niños ya estaban en la cama, la cruel madrastra le dijo al leñador: 'No hay suficiente pan para todos. Mañana llevaremos a los niños al bosque y los dejaremos allí'.")
+print("El padre, de corazón bondadoso, no quería hacerlo, pero la madrastra insistió hasta que él aceptó con tristeza.")
+print("Hansel que no podía dormir, escuchó la conversación y decidió hacer algo para salvarse. Fue como a la mañana siguiente, salió en silencio al jardín y llenó sus bolsillos con piedritas blancas que brillaban")
+print("Muy temprano, la madrastra los despertó, les dio un pedazo de pan duro y los llevó con su padre al bosque profundo.")
+print("Mientras caminaban, Hansel iba dejando caer una piedrita cada pocos pasos, marcando el camino de regreso a casa.")
