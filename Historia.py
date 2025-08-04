fb755b61fb8c2d744f91a908735474791d51d413
Natalia Hernandez <natyhr27@gmail.com>
2025-08-03 21:52:11 -0600

    Agrega parte 2 de la historia

--- Historia.py
+++ Historia.py
@@ -6,3 +6,17 @@ print("El padre, de corazón bondadoso, no quería hacerlo, pero la madrastra in
 print("Hansel que no podía dormir, escuchó la conversación y decidió hacer algo para salvarse. Fue como a la mañana siguiente, salió en silencio al jardín y llenó sus bolsillos con piedritas blancas que brillaban")
 print("Muy temprano, la madrastra los despertó, les dio un pedazo de pan duro y los llevó con su padre al bosque profundo.")
 print("Mientras caminaban, Hansel iba dejando caer una piedrita cada pocos pasos, marcando el camino de regreso a casa.")
+
+print (
+    '''
+    Al llegar, su padre los recibió con lágrimas en los ojos, pero la madrastra estaba furiosa. 
+    Sin embargo, al poco tiempo, la escasez volvió a acechar, y la madrastra insistió en llevarlos de nuevo al
+    bosque.
+
+    Esta vez, Hansel no pudo recoger piedritas, porque la puerta estaba cerrada. En su lugar, dejó caer migajas 
+    de pan para marcar el camino. Pero para desgracia de los niños, los pájaros del bosque se comieron las migajas 
+    y quedaron perdidos.
+    Perdidos y asustados, caminaron hasta que encontraron una casa hecha de pan de jengibre, dulces y caramelos. 
+    Hambrientos, comenzaron a comer la casa sin saber que dentro vivía una vieja bruja malvada que los atrapó...
+    '''
+)
